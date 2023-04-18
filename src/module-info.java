5d8bfbb7bd636d52aa9d8d4198d34672c8fdf8ff
jiyoung park <miracle48693@naver.com>
2023-04-18 23:27:39 +0900

    clear git cache

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mirac
+ *
+ */
+module teamproject {
+}
---
+++ src/project/Test.java
@@ -0,0 +1,9 @@
+package project;
+
+public abstract class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello E&I");
+	}
+
+}
