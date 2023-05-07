1d4a7563222cf57a2c3bda0d2cb7d4718efdd52f
jiyoung3725 <130877885+jiyoung3725@users.noreply.github.com>
2023-05-07 12:28:57 +0900

    Delete src/customerservice directory

--- src/customerservice/Tap_Customeropinion.java
+++
@@ -1,7 +0,0 @@
-package customerservice;
-
-import javax.swing.JPanel;
-
-public class Tap_Customeropinion extends JPanel {
-
-}
--- src/customerservice/Tap_FrequentlyQuestion.java
+++
@@ -1,7 +0,0 @@
-package customerservice;
-
-import javax.swing.JPanel;
-
-public class Tap_FrequentlyQuestion extends JPanel {
-
-}
