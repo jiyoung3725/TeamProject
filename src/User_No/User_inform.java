304949923a315c1dc1962705b8c364757a076c30
SIST <SIST@DESKTOP-I3PS9RG>
2023-05-08 14:49:04 +0900

    0508_14:48_수진_게시글 작성자 정보 불러오기 기능 코드 업로드

---
+++ src/User_No/User_inform.java
@@ -0,0 +1,38 @@
+package User_No;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import boards.Board;
+import db.ConnectionProvider;
+
+//선택된 게시글의 user정보를 가져오는 클래스
+public class User_inform {
+	
+	public HashMap<String, Object> userInform() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+//		B_no b_no = new B_no();
+//		int b_no_selected = b_no.boardNo();
+		int b_no_selected = Board.postNum;
+		String sql = "SELECT b_no, b.user_no, user_id "
+				+ "FROM board b, user_info u "
+				+ "WHERE b.user_no=u.user_no and b_no="+b_no_selected;
+		try {
+			Connection conn = ConnectionProvider.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				map.put("b_no", rs.getInt(1));
+				map.put("user_no", rs.getInt(2));
+				map.put("user_id", rs.getString(3));
+			}
+		}catch (Exception e) {
+			System.out.println("예외:"+e.getMessage());
+		}
+		return map;
+	}
+
+}
