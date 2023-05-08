fe91e04ccb2c3050ec355640d18b31f84e8c4fd6
SIST <SIST@DESKTOP-I3PS9RG>
2023-05-08 14:47:15 +0900

    0508_14:47_수진 수정

--- src/boards/Board.java
+++ src/boards/Board.java
@@ -3,8 +3,10 @@ package boards;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.Console;
@@ -29,6 +31,7 @@ import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableRowSorter;
 
+import Post.post_Reader;
 import dao.BoardDAO;
 import vo.BoardVO;
 
