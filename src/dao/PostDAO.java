8e0df93d6ac0ff2e18e42325ef10fb1dfdc6f71e
Sujin524-Jeon <131145552+Sujin524-Jeon@users.noreply.github.com>
2023-05-09 11:39:03 +0900

    PostDAO_0509_11:38

--- src/dao/PostDAO.java
+++ src/dao/PostDAO.java
@@ -1,20 +1,40 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Vector;
 
-import boards.Board;
+import boards.Board_0509;
 import db.ConnectionProvider;
-
-
+import first.LogInPage;
+import vo.BoardVO;
+import vo.PostVO;
 
 public class PostDAO {
 	//게시글 삭제
 	public int deletePost() {
 		int re = -1;
-		String sql = "DELETE FROM board WHERE b_no="+Board.postNum;
+		String sql = "DELETE FROM board WHERE b_no="+Board_0509.postNum;
+		try {
+			Connection conn = new ConnectionProvider().getConnection();
+			Statement stmt = conn.createStatement();
+			re = stmt.executeUpdate(sql);
+			ConnectionProvider.close(stmt, conn);
+		} catch (Exception e) {
+			System.out.println("예외:"+e.getMessage());
+		}
+		return re;		
+	}
+	//게시글 수정
+	public int updatePost(PostVO p) {
+		int re = -1;
+		String sql = "UPDATE board SET category='"+p.getCategory()+"', interest='"+p.getInterest()+"', "
+				+ "application='"+p.getApplication()+"', "
+				+ "title='"+p.getTitle()+"', b_content='"+p.getB_content()+"' "
+				+ "WHERE b_no="+p.getB_no();
 		try {
 			Connection conn = new ConnectionProvider().getConnection();
 			Statement stmt = conn.createStatement();
@@ -26,19 +46,53 @@ public class PostDAO {
 		return re;		
 	}
 	
-	//게시글 수정
-	public int updatePost() {
-		int re = -1;
-		String sql = "";
+	//게시글 수정시 기존 값을 띄우기 위한 메소드
+	public ArrayList<Object> setDate(){
+		ArrayList<Object> listData = new ArrayList<Object>();
+		String sql = "SELECT category, interest, application, title, b_content "
+				+ "FROM board "
+				+ "WHERE b_no="+Board_0509.postNum;
+		
+		int idx_cat = -1;
+		int idx_interest = -1;
+		
 		try {
-			Connection conn = new ConnectionProvider().getConnection();
+			Connection conn = ConnectionProvider.getConnection();
 			Statement stmt = conn.createStatement();
-			re = stmt.executeUpdate(sql);
-			ConnectionProvider.close(stmt, conn);
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				if(rs.getString(1).equals("함께해요")) {
+					idx_cat=0;
+				}else {
+					idx_cat=1;
+				}
+				if (rs.getString(2).equals("건강/운동")) {
+					idx_interest=0;
+				}else if (rs.getString(2).equals("음식/요리")) {
+					idx_interest=1;
+				}else if (rs.getString(2).equals("영화/공연/전시")) {
+					idx_interest=2;
+				}else if (rs.getString(2).equals("미술/공예")) {
+					idx_interest=3;
+				}else if (rs.getString(2).equals("노래/음악")) {
+					idx_interest=4;
+				}else if (rs.getString(2).equals("재테크")) {
+					idx_interest=5;
+				}else {
+					idx_interest=6;
+				}
+				listData.add(idx_cat);	//카테고리 콤보박스 인덱스
+				listData.add(idx_interest);	// 관심사 콤보박스 인덱스
+				listData.add(rs.getString(3));	//신청인원
+				listData.add(rs.getString(4));	//제목
+				listData.add(rs.getString(5));	//내용	
+			}			
+			ConnectionProvider.close(rs, stmt, conn);
 		} catch (Exception e) {
-			System.out.println("예외:"+e.getMessage());
+			System.out.println("예외발생:"+e.getMessage());
 		}
-		return re;		
+		return listData;		
 	}
 	
 //	public ArrayList<PostVO> showPost(){
